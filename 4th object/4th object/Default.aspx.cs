0c48ba41e9f9fa06bff707dcdf4944f3faef5aaa
blackcatrecycler <1121042683@qq.com>
2015-04-06 17:08:03 +0800

    finally

---
+++ 4th object/4th object/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        CKFinder.FileBrowser fileBrowser = new CKFinder.FileBrowser();
+        fileBrowser.BasePath = "/ckfinder/";  //设置CKFinder的基路径  
+        fileBrowser.SetupCKEditor(CKEditorControl1);
+        
+    }
+    protected void CKEditorControl1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+}
